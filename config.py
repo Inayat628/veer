6ddfb3cc71d56ddf2b0277b26a586f0baa0345f7
Inayat628 <kraj372062@gmail.com>
2024-11-18 20:23:04 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,40 @@
+#API_SAUKE
+#SASUKE_FOEMAT
+#BYSasUKE_API_KEY FAVDAUTCREHY
+#OWNER - @BeasTxt_Sasuke
+#KEY_REDEMPTION_1711
+#ACCESS_KEY_1711
+#FAILED_SasUKE_1711
+#HFAHYOYO_YTIATII_1575757_AS4575AA_57A7ASUYUT_WUUTIUU_4687_1711
+#JOIN - @bgmisellingbuying
+#CHANNEL- @StoRm_BGMI_HACKS_STORE
+
+# Don't use quotes( " and ' )
+#SCRIPT BY SASUKE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7279340956:AAEvhEvL8tF2-46e7lsUjRs93Unqp0ym8HA")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@PLAYMODYASH")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5207555674")
+
+#API_SAUKE
+#SASUKE_FOEMAT
+#BYSasUKE_API_KEY FAVDAUTCREHY
+#OWNER - @BeasTxt_Sasuke
+#KEY_REDEMPTION_1711
+#ACCESS_KEY_1711
+#FAILED_SasUKE_1711
+#HFAHYOYO_YTIATII_1575757_AS4575AA_57A7ASUYUT_WUUTIUU_4687_1711
+#JOIN - @bgmisellingbuying
+#CHANNEL- @StoRm_BGMI_HACKS_STORE
+
+
+
+
+
+
+  
